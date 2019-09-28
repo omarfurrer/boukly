275ba7f7fb16057e36bdcfc3f16ec643cfbbf8c5
Omar Furrer <omar.furrer@gmail.com>
2019-09-28 14:51:29 +0200

    Add chat widget

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -72,10 +72,23 @@
                 </ul>
             </div>
         </nav>
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
     </div>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5d8e8851db28311764d63db0/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
