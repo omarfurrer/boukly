c46f5e3b132cb7bbef5f3442c0bdf6cd781bd2db
Omar Furrer <omar.furrer@gmail.com>
2019-09-28 14:51:11 +0200

    Update landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,11 +2,23 @@
 
 @section('content')
 <div id="landing">
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-3">Boukly.io</h1>
+            <p>Boukly.io is a free online application that I created to help solve the problem of organizing bookmarks.
+                Sign up, download the chrome extension, start using Boukly and enjoy bookmarking again.
+            </p>
+            <p>I am always improving Boukly. If you have any questions or want new features added, please contact me using the button below.</p>
+            <p><a
+                    class="btn btn-primary btn-lg"
+                    href="javascript:void(Tawk_API.toggle())"
+                    role="button"
+                >Contact Me »</a></p>
+        </div>
+    </div>
     <div class="container">
-
         <div class="main text-center">
-            <h1>Boukly.io</h1>
-            <h3>Private Bookmarking For Your Needs</h3>
+            <h1>Sign Up</h1>
         </div>
         <div class="cta mt-5">
             <form
@@ -82,56 +94,63 @@
                             type="submit"
                             class="btn btn-primary btn-block"
                         >
-                            {{ __('Register') }}
+                            Go
                         </button>
                     </div>
                 </div>
             </form>
         </div>
+
     </div>
-    <div class="container-fluid">
-        <div class="main-features mt-5 text-center">
-            <div class="row">
-                <div class="col mx-3">
-                    {{-- <div class="mx-2"> --}}
-                    <h4>Privacy</h5>
-                        <h5>With Boukly you can mark your bookmarks as private and they will be hidden in your
-                            dashboard
-                            untill you provide your password.</h5>
-                        {{-- </div> --}}
-                </div>
-                <div class="col mx-3">
-                    <h4>Cloud</h5>
-                        <h5>Boukly keeps your bookmarks stored securely on the cloud so you will never have to lose them
-                            again while having the ability to access them from anywhere.</h5>
-                </div>
-                <div class="col mx-3">
-                    <h4>Organize</h5>
-                        <h5>You can add tags to your bookmarks to organize them. Boukly will also attempt to tag your
-                            boomarks for you.</h5>
-                </div>
-            </div>
-            <div class="row mt-5">
-                <div class="col mx-3">
-                    {{-- <div class="mx-2"> --}}
-                    <h4>Privacy</h5>
-                        <h5>With Boukly you can mark your bookmarks as private and they will be hidden in your
-                            dashboard
-                            untill you provide your password.</h5>
-                        {{-- </div> --}}
-                </div>
-                <div class="col mx-3">
-                    <h4>Cloud</h5>
-                        <h5>Boukly keeps your bookmarks stored securely on the cloud so you will never have to lose them
-                            again while having the ability to access them from anywhere.</h5>
-                </div>
-                <div class="col mx-3">
-                    <h4>Organize</h5>
-                        <h5>You can add tags to your bookmarks to organize them. Boukly will also attempt to tag your
-                            boomarks for you.</h5>
-                </div>
+
+    <div class="container-fluid example-section">
+        <div class="row justify-content-lg-center">
+            <div class="col-lg-8">
+                <img
+                    src="/images/example.png"
+                    class="img-fluid mt-5"
+                    alt="Responsive image"
+                >
             </div>
         </div>
     </div>
+    <div class="container mt-5 mb-5">
+
+        <h1 class="text-center">Features</h1>
+
+        <div class="row mt-5">
+            <div class="col-md-4">
+                <h2>Cloud</h2>
+                <p>Boukly keeps your bookmarks stored securely on the cloud so you will never have to lose them
+                    again while having the ability to access them from anywhere.</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Organize</h2>
+                <p>Boukly tries to organize your bookmarks for you by sorting them into different tags according to the
+                    domain</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Adult</h2>
+                <p>Boukly will automatically detect if a bookmark has adult content and mark it as private</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-4">
+                <h2>Info</h2>
+                <p>Boukly will try to extract as much info from the bookmark as possible and display it on your
+                    dashboard</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Tags</h2>
+                <p>You can filter your bookmarks by tags and you can even search through the tags themselves</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Broken</h2>
+                <p>Boukly will try and identify broken links for you that are no longer working and notify you about
+                    them</p>
+            </div>
+        </div>
+    </div>
+
 </div>
 @endsection
